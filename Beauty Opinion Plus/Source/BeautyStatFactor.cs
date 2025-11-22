695e3e9d748c933556ce28309d278ac5dbbaa866
DXwer <42659696+DXwer@users.noreply.github.com>
2025-11-21 17:01:21 -0700

    added Beauty Opinion Plus

---
+++ Beauty Opinion Plus/Source/BeautyStatFactor.cs
@@ -0,0 +1,72 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BeautyOpinionMod
+{
+    public class ThoughtWorker_Pretty : ThoughtWorker
+    {
+      protected override ThoughtState CurrentSocialStateInternal(Pawn pawn, Pawn other)
+      {
+            if (!other.RaceProps.Humanlike || !RelationsUtility.PawnsKnowEachOther(pawn, other))
+            {
+                return false;
+            }
+            if (RelationsUtility.IsDisfigured(other, pawn))
+            {
+                return false;
+            }
+            if (PawnUtility.IsBiologicallyOrArtificiallyBlind(pawn))
+            {
+                return false;
+            }
+            float statValue = other.GetStatValue(StatDefOf.PawnBeauty);
+            if (statValue >= 10f)
+            {
+                return ThoughtState.ActiveAtStage(9);
+            }
+            if (statValue >= 9f)
+            {
+                return ThoughtState.ActiveAtStage(8);
+            }
+            if (statValue >= 8f)
+            {
+                return ThoughtState.ActiveAtStage(7);
+            }
+            if (statValue >= 7f)
+            {
+                return ThoughtState.ActiveAtStage(6);
+            }
+            if (statValue >= 6f)
+            {
+                return ThoughtState.ActiveAtStage(5);
+            }
+            if (statValue >= 5f)
+            {
+                return ThoughtState.ActiveAtStage(4);
+            }
+            if (statValue >= 4f)
+            {
+                return ThoughtState.ActiveAtStage(3);
+            }
+            if (statValue >= 3f)
+            {
+                return ThoughtState.ActiveAtStage(2);
+            }
+            if (statValue >= 2f)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            if (statValue >= 1f)
+            {
+                return ThoughtState.ActiveAtStage(0);
+            }
+            return false;
+        }
+    }
+}
---
+++ Beauty Opinion Plus/Source/BeautyStatMod.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Verse.Noise;
+
+namespace BeautyOpinionMod
+{
+    [StaticConstructorOnStartup]
+    public static class BeautyOpinionMods
+    {
+        
+        static BeautyOpinionMods()
+        {
+            var harmony = new Harmony("beautyopinionmod.caravan.patch");
+            harmony.PatchAll();
+
+            
+        }
+
+    }
+
+}
